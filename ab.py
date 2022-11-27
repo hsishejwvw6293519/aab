a521d8425fbaab6bd16ae3685dcbdca0ee49ea77
hsishejwvw6293519 <119119738+hsishejwvw6293519@users.noreply.github.com>
2022-11-27 15:53:27 +0600

    Update ab.py

--- ab.py
+++ ab.py
@@ -10,7 +10,7 @@ def upt(u,c):
     u.message.reply_text(a)
 
 
-uu = Updater("5895389339:AAFxtHGcFItvazTYkYiSDN2EARcRluWVE9U",use_context=True)
+uu = Updater("5895389339:AAHKPlTe1Tp1meGvgMuQzwBIuJWB7drR7qo",use_context=True)
 
 uu.dispatcher.add_handler(CommandHandler("start",stm))
 uu.dispatcher.add_handler(MessageHandler(Filters.photo,upt))
